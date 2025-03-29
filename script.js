6b3935c6a314d046f7fe287cdb7d4f7b7eab8ce8
jmald1987 <77485004+jmald1987@users.noreply.github.com>
2025-03-29 08:42:25 -0400

    Add files via upload
    fixed header and window.

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ var modal = document.getElementById("modal");
 var body = document.getElementById("body");
 var btn = document.getElementById("modal_btn");
 var span = document.getElementsByClassName("close")[0];
+var first_row=document.getElementById("first_row");
+var second_row=document.getElementById("second_row");
 
 
 btn.onclick = function() { 
-modal.style.display = "block";
-body.style.zIndex = "-1";
-modal.style.zIndex = "1";
+  modal.style.display = "flex";
+  first_row.style.display= "none";
+  second_row.style.display= "none";
 }
 
 span.onclick = function() {
   modal.style.display = "none";
-  body.style.zIndex = "1";
-  modal.style.zIndex = "-1";
+  first_row.style.display= "flex";
+  second_row.style.display= "flex";
 }
 
 
